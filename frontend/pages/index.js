1e3f231a0b644bca4065c6c54503368da5c5bfd6
fancyaction <labcoatdisaster@gmail.com>
2018-11-06 15:01:18 -0600

    Setup Page and custom app

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,8 @@
-import Link from 'next/link';
-
 const Home = props => (
     <div>
         <p>Greetings!</p>
-        <Link href="/sell">
-            <a>Sell!</a>
-        </Link>
     </div>
 );
+
 
 export default Home;
