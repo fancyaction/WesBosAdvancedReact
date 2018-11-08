1f6d94e8af62ed7ea69f5b91681d1f6c607fff0c
Allistair <allistair.everett@fracing.com>
2018-11-08 17:51:53 -0600

    Setup server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+
+
+
+const server = createServer();
+
+// TODO Use middleware to handle JWT
+// TODO User middleware to populate current user
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    },
+},
+    details => {
+        console.log(`Server now running on port http:/localhost:${details.port}`)
+});
