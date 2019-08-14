74c69c7a4638e513371b796eca2aab396cde04e5
fancyaction <labcoatdisaster@gmail.com>
2019-08-14 17:59:26 -0500

    Setup apollo in app

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,7 +2,7 @@ const { forwardTo } = require('prisma-binding');
 
 
 const Query = {
-    items: forwardTo('db'),
+    items: forwardTo('db'), // Note: No custom logic necessary
     // async items(parent, args, ctx, info) {
     //     const items = await ctx.db.query.items();
 
