8b0efff7529dfbd45942779b38b2f78e3cc62ac8
fancyaction <labcoatdisaster@gmail.com>
2019-10-31 17:57:16 -0500

    Setup SignupPage

---
+++ frontend/components/Signup.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import Error from './ErrorMessage';
+
+export default class Signup extends Component {
+    render() {
+        return (
+            <Form>
+                <fieldset>
+                    <h2>Sign up for an account</h2>
+                </fieldset>
+            </Form>
+        )
+    }
+}
